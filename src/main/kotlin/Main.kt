f34df07952b363fa5e7545204e295ff1b46e3e93
gpeck578 <gpeck578@gmail.com>
2024-02-26 21:16:24 -0500

    Added timers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,20 +9,23 @@ private const val BASE_URL = "http://kotlin-book.bignerdranch.com/2e"
 private const val FLIGHT_ENDPOINT = "$BASE_URL/flight"
 private const val LOYALTY_ENDPOINT = "$BASE_URL/loyalty"
 
-suspend fun fetchFlight(passengerName: String): FlightStatus = coroutineScope {
+suspend fun fetchFlight(
+    passengerName: String,
+    start: Long
+): FlightStatus = coroutineScope {
     val client = HttpClient(CIO)
 
     val flightResponse = async {
-        println("Started fetching flight info")
+        println("Started fetching flight info: ${System.currentTimeMillis() - start}")
         client.get<String>(FLIGHT_ENDPOINT).also {
-            println("Finished fetching flight info")
+            println("Finished fetching flight info: ${System.currentTimeMillis() - start}")
         }
     }
 
     val loyaltyResponse = async {
-        println("Started fetching loyalty info")
+        println("Started fetching loyalty info: ${System.currentTimeMillis() - start}")
         client.get<String>(LOYALTY_ENDPOINT).also {
-            println("Finished fetching loyalty info")
+            println("Finished fetching loyalty info: ${System.currentTimeMillis() - start}")
         }
     }
     delay(500)
@@ -36,12 +39,14 @@ suspend fun fetchFlight(passengerName: String): FlightStatus = coroutineScope {
 }
 
 fun main() {
+    val start = System.currentTimeMillis()
+
     runBlocking {
-        println("Started")
+        println("Started: ${System.currentTimeMillis() - start}")
         launch {
-            val flight = fetchFlight("Madrigal")
+            val flight = fetchFlight("Madrigal", start)
             println(flight)
         }
-        println("Finished")
+        println("Finished: ${System.currentTimeMillis() - start}")
     }
 }
